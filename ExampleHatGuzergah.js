6af6cd4de9a476e9bfb08a7ce3783e2143c7bff5
Sophia Brooks <scross@finnacloud.com>
2024-08-04 15:15:38 +0300

    fix goofy tabs-

--- ExampleHatGuzergah.js
+++ ExampleHatGuzergah.js
@@ -1,10 +1,10 @@
 import {HatGuzergah} from "./HatGuzergah.js";
 
 (async () => {
-	const hatGuzergah = new HatGuzergah();
-	try {
-		console.log(await hatGuzergah.hatSorgula({HatKodu: 151}));
-	} catch (error) {
-		console.error('Error:', error);
-	}
+  const hatGuzergah = new HatGuzergah();
+  try {
+    console.log(await hatGuzergah.hatSorgula({HatKodu: 151}));
+  } catch (error) {
+    console.error('Error:', error);
+  }
 })();
